68e9dea2e689ba169c87d5dcb6803ff87bd10978
balagee <balagee1005@gmail.com>
2021-03-19 15:01:48 +0530

    task

---
+++ src/test/java/org/pro/Sample.java
@@ -0,0 +1,27 @@
+package org.pro;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+	
+ public WebDriver driver;
+ public void getdriver() {
+	 WebDrivermanager.chromedriver().setup();
+ }
+ 
+	
+	
+			
+				
+			 
+				
+			
+			
+
+			
+		
+	}
+	
+
